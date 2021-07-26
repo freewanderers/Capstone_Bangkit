5a1c741db88876574abc63ecb96a4c7849fc1c78
freewanderers <freewanderer.fw@gmail.com>
2021-07-26 20:09:23 +0700

    android layout change

--- run.py
+++ run.py
@@ -61,5 +61,5 @@ def result():
     return render_template('result.html', result=result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-    # app.run(debug=True)
+    # app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
